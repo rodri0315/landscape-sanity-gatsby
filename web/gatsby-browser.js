6c03df92e18361bb551a8ab362418eee10f97519
rodri0315 <rodri0315@gmail.com>
2021-08-20 01:22:46 -0400

    Initial commit, Add nav and header.

---
+++ web/gatsby-browser.js
@@ -0,0 +1,18 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+// import "bootstrap/dist/css/bootstrap.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import './src/styles/layout.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.min.js";
+// import "@popperjs/core/dist/umd/popper.min.js";
+
+// import "~bootstrap/dist/css/bootstrap"
+
+// @import '~react-select/dist/react-datetime.css';
+// @import '~bootstrap/dist/css'
---
+++ web/src/components/header.js
@@ -0,0 +1,58 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import Icon from './icons'
+import { cn } from '../lib/helpers'
+import Button from 'react-bootstrap/Button';
+
+
+import styles from './header.module.css'
+import { NavDropdown, Nav, Navbar, Container } from 'react-bootstrap';
+
+const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
+  <>
+    <Navbar bg="light" expand="lg" id="menu">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <header id='header'>
+      <div className='intro'>
+        <div className='overlay'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-8 offset-md-2 intro-text'>
+                <h1>
+                  J&M Lawn Service
+                  <span></span>
+                </h1>
+                <p>Full-service company that aims to help your lawn and landscape look its best.</p>
+                <a
+                  href='#features'
+                  className='btn btn-custom btn-lg page-scroll'
+                >
+                  Learn More
+                </a>{' '}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </>
+)
+
+export default Header
